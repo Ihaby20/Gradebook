95879507b51e81892877033be4f2292af212b8e3
Ihaby20 <108707415+Ihaby20@users.noreply.github.com>
2022-07-06 20:03:09 +0100

    Add files via upload

---
+++ Gradebook.py
@@ -0,0 +1,25 @@
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+subjects = ["physics", "calculus", "poetry", "history"]
+grades = [98, 97, 85, 88]
+
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+print(gradebook)
+
+gradebook.append(["computer science", 100])
+print(gradebook)
+
+gradebook.append(["visual arts", 93])
+
+gradebook[5][1] = 98
+print(gradebook)
+
+gradebook[2].remove(85)
+print(gradebook)
+
+gradebook[2].append("Pass")
+
+
+full_gradebook = gradebook + last_semester_gradebook
+print(full_gradebook)
